07089c6a9ff70a4f5226740d92c41e52b7e11bd6
Umeadi, Marlon <umeadi@gmail.com>
2019-10-03 12:19:18 -0500

    copied file structure from Updated portfolio

---
+++ javascript/portfolio.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // carousel settings
+    var interval;
+    
+    $("#carouselExampleIndicators").carousel({interval: 2000, wrap: true})
+    var interval = interval;
+    console.log("----------------------------");
+    console.log("Carousel Speed: " + interval + " milliseconds");
+});
